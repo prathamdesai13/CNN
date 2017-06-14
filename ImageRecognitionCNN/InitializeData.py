7002c556af6e51f2aaa7b7d420cc45de467b3498
Pratham Desai <prathamdesai13@gmail.com>
2017-06-13 23:02:39 -0400

    Add files via upload

--- ImageRecognitionCNN/InitializeData.py
+++ ImageRecognitionCNN/InitializeData.py
@@ -3,6 +3,7 @@ Initialize mnist data and split for training and validation
 """
 import numpy as np
 import sklearn.datasets as sk
+import random
 
 
 def initialize():
@@ -10,11 +11,20 @@ def initialize():
     data = sk.fetch_mldata('MNIST original', data_home='./MNIST')
 
     split = 60000
-    inputs = np.reshape(data.data[:split], (-1, 1, 28, 28))/255.0
+    inputs = np.reshape(data.data[:split], (-1, 1, 28, 28)).astype(np.uint64)
     labels = data.target[:split]
-    validation_inputs = np.reshape(data.data[split:], (-1, 1, 28, 28))/255.0
+    validation_inputs = np.reshape(data.data[split:], (-1, 1, 28, 28)).astype(np.uint64)
     validations_labels = data.target[split:]
     num_classes = np.unique(labels).shape[0]
+
+    tuple = []
+    for i in range(inputs.shape[0]):
+        tuple.append((inputs[i], labels[i]))
+
+    random.shuffle(tuple)
+    for i in range(len(tuple)):
+        inputs[i] = tuple[i][0]
+        labels[i] = tuple[i][1]
 
     return inputs, labels, validation_inputs, validations_labels, num_classes
 
