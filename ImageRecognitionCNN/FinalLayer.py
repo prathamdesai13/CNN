f7a0d46157710f70d81db948dd0d5b88479b06d8
Pratham Desai <prathamdesai13@gmail.com>
2017-06-19 20:47:24 -0400

    Add files via upload

--- ImageRecognitionCNN/FinalLayer.py
+++ ImageRecognitionCNN/FinalLayer.py
@@ -12,36 +12,32 @@ class FinalLayer(Layer):
 
     def __init__(self):
         self.input_vector = None
-        self.scores = None
+        self.error = None
 
     def update_parameters(self, learning_rate):
         pass
 
     def forwards_pass(self, input_vector):
-        self.input_vector = input_vector
 
-        exp = np.exp(input_vector - np.amax(input_vector, axis=1, keepdims=True))
-        sum_exp = np.sum(exp, axis=1, keepdims=True)
-        norm_exp = (exp / sum_exp)
-        self.scores = norm_exp
-
+        input_vector = input_vector - np.max(input_vector)
+        norm_exp = (np.exp(input_vector).T / np.sum(np.exp(input_vector), axis=1)).T
+        epsilon = 1e-3
+        norm_exp = np.clip(norm_exp, epsilon, 1 - epsilon)
         return norm_exp
 
     def backwards_pass(self, output_gradient):
-        pass
 
-    def gradient(self, output_vector_prediction, output_vector_label):
-
-        gradient = output_vector_prediction - output_vector_label
+        gradient = output_gradient * self.error
 
         return gradient
 
+    def gradient(self, output_vector_prediction, output_vector_label):
+
+        self.error = output_vector_label - output_vector_prediction
+
     def loss(self, output_vector_prediction, output_vector_label):
 
-        epsilon = 1e-15
         N = output_vector_label.shape[0]
-        output_vector_prediction = np.clip(output_vector_prediction, epsilon, 1 - epsilon)
-        output_vector_prediction /= output_vector_prediction.sum(axis=1, keepdims=True)
         soft_loss = -np.sum(output_vector_label * np.log(output_vector_prediction))
         norm_soft_loss = soft_loss / N
 
--- ImageRecognitionCNN/HiddenLayer.py
+++ ImageRecognitionCNN/HiddenLayer.py
@@ -10,33 +10,28 @@ class HiddenLayer(Layer):
     def __init__(self, num_classes, num_inputs):
 
         self.input = None
-        self.weights = np.random.normal(scale=0.1, size=(num_inputs, num_classes))
-        self.bias = np.random.normal(scale=0.1, size=num_classes)
+        self.weights = np.zeros((num_classes, num_inputs))
         self.weights_gradient = np.zeros(self.weights.shape)
-        self.bias_gradient = np.zeros(self.bias.shape)
 
     def forwards_pass(self, input_vector):
 
         self.input = input_vector
 
-        weighted_sum = np.dot(self.input, self.weights) + self.bias
+        weighted_sum = np.dot(self.input, self.weights.T)
 
         return weighted_sum
 
     def backwards_pass(self, output_gradient):
-
-        N = output_gradient.shape[0]
-        self.weights_gradient = np.dot(self.input.T, output_gradient)
-        self.weights_gradient /= N
-        self.weights_gradient -= 0.02 * self.weights
-        self.bias_gradient = np.mean(output_gradient, axis=0)
-        input_gradient = np.dot(output_gradient, self.weights.T)
+        N = self.input.shape[0]
+        self.weights_gradient = -np.dot(self.input.T, output_gradient)
+        self.weights_gradient = self.weights_gradient / N
+        self.weights_gradient = self.weights_gradient.T
+        input_gradient = np.dot(output_gradient, self.weights)
 
         return input_gradient
 
     def update_parameters(self, learning_rate):
 
-        self.weights -= self.weights_gradient * learning_rate
-        self.bias -= self.bias_gradient * learning_rate
+        self.weights = self.weights - (learning_rate * self.weights_gradient)
 
 
