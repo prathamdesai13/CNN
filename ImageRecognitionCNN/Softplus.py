626bd775a307a5283561f3437f9f2c14b30b604b
Pratham Desai <prathamdesai13@gmail.com>
2017-06-22 00:19:46 -0400

    Add files via upload

---
+++ ImageRecognitionCNN/Softplus.py
@@ -0,0 +1,36 @@
+"""
+Softplus activations function
+"""
+
+"""
+ReLU for convolutions in CNN
+"""
+import numpy as np
+from Layer import Layer
+
+
+class ReLU(Layer):
+
+    def __init__(self):
+
+        self.input = None
+        
+    def forwards_pass(self, input_vector):
+        self.input = input_vector
+        unit = np.log(1 + np.exp(self.input))
+        return unit
+
+    def backwards_pass(self, output_gradient):
+
+        input_gradient = output_gradient * self.softplus_gradient(self.input)
+
+        return input_gradient
+
+    def update_parameters(self, learning_rate):
+        pass
+
+    def softplus_gradient(self, input):
+
+        dx = 1.0 / (1.0 + np.exp(-input))
+
+        return dx
